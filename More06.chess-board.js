4a2494f306aef81500cec4b58be1ae0338f421d0
Eva Kostadinova <e.ts.kostadinova@gmail.com>
2025-09-27 22:46:42 +0300

    Add files via upload

---
+++ More06.chess-board.js
@@ -0,0 +1,24 @@
+function chessBoard(n) {
+    let result = '<div class="chessboard">\n';
+
+    for(let row = 0; row < n; row++){
+        result += '  <div>\n';
+
+        for(let col = 0; col < n; col++){
+            const isBlack = (row + col)% 2 === 0;
+            const colorClass = isBlack ? 'black' : 'white';
+            result += `    <span class="${colorClass}"></span>\n`;
+        }
+
+        result += '  </div>\n';
+
+        }
+
+    result += '</div>';
+    return result;
+    
+    }
+
+
+
+console.log(chessBoard(3));
